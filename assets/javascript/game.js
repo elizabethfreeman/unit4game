5edd3ff6ef1ec6c2bf338bbb91143705bf4005a6
lizfreeman <lizfreeman7@gmail.com>
2019-10-10 22:15:19 -0400

    most html complete, began work on javescript and jquery, pseudocoded operations

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+//Global variables
+//3 different categories things we're tracking, value of crystals each round, the score for each round, the win and loss counter
+
+var blueCrystal = 0
+var redCrystal = 0
+var greenCrystal =0 
+var purpleCrystal = 0
+
+// var crystal = {
+//     blue:
+//     {name : "Blue",}
+// }
+
+// create  a variable called target
+var target; //grabbing the glass for the whiskey
+
+// generate a random number and assign it to target
+target = Math.floor(Math.random() * (150 - 90) ) + 90; // returns a random integer from 90 to 150
+
+// show target number on screen
+// reach into the html and grab the targetScore element 
+    $("#targetScore").html(target)
+// assign our randomNumber to that targetScore element
+
+// console.log(target)
+// when a person click a gem 
+$(".gems").on("click",function(){
+var chosenCrystal=$(this).attr("alt")
+console.log(chosenCrystal)
+    // get the value of the gem
+    // add that value to the your score
+})
+
+
+//Functions
+//
+
+
+//Main Process
+//
